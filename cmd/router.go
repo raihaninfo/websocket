f86d72e5e7f84afc4cdcaaab8bad34ad5dbedb4e
raihaninfo <raihanmahmudi35@gmail.com>
2022-11-12 20:03:38 +0600

    static file

--- cmd/router.go
+++ cmd/router.go
@@ -10,7 +10,7 @@ func (a *application) router() http.Handler {
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", a.welcome).Methods("GET")
 	mux.HandleFunc("/ws", a.WsEndpoint).Methods("GET")
-	mux.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	mux.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./static"))))
 	http.ListenAndServe(":8080", mux)
 
 	return mux
